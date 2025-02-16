11941f4edc8a6f4e84223ca8e66b9b893cb3cf25
ip <ivan.poluianov@gmail.com>
2025-02-16 13:37:53 +0100

    Revert "it works in macos"
    This reverts commit a78f3bb1f58ba5c613b2db916051406c62071b05.

--- lib/backend.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:ffi';
-
-final DynamicLibrary nativeLib = DynamicLibrary.open('libbackend.dylib');
-
-typedef IncrementFunc = Int32 Function(Int32);
-typedef Increment = int Function(int);
-final Increment increment =
-    nativeLib.lookup<NativeFunction<IncrementFunc>>('Increment').asFunction();
